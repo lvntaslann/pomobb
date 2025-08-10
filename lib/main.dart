ea0102923c9a58f1414dbcd66c4528be5518132b
Levent Aslan <lvnt.aslann@gmail.com>
2025-08-10 12:35:13 +0300

    Etkinlik ekleme işlemi için UI içeriği hazırlandı ve fonksiyon eklendi. Eklenen etkinliği silme fonksiyonu eklendi.

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return  MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Home()
     );
--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -2,20 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pomobb/cubit/content/content_cubit.dart';
 import 'package:pomobb/cubit/content/content_state.dart';
+import 'package:pomobb/model/content_model.dart';
 import '../../themes/app_colors.dart';
 import '../../widgets/container/build_timer_container.dart';
 
 class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
+  Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    TextEditingController subjectController = TextEditingController();
+    TextEditingController timeController = TextEditingController();
     return Scaffold(
       backgroundColor: AppColors.homeBgColor,
       body: BlocBuilder<ContentCubit, ContentState>(
-        
         builder: (context, state) {
-
           if (state.isLoading) {
             return const Center(child: CircularProgressIndicator());
           }
@@ -24,7 +25,7 @@ class Home extends StatelessWidget {
             return const Center(
               child: Text(
                 "Henüz içerik eklenmemiş",
-                style: TextStyle(color: Colors.white, fontSize: 18),
+                style: TextStyle(fontSize: 18),
               ),
             );
           }
@@ -50,11 +51,15 @@ class Home extends StatelessWidget {
                       final item = state.allContent[index];
                       return Padding(
                         padding: const EdgeInsets.only(bottom: 20),
-                        child: BuildTimerContainer(
-                          subject: item.subject,
-                          time: "${item.time}dk",
-                          iconPath: item.iconPath,
-                          containerBg: item.containerBgColor,
+                        child: GestureDetector(
+                          onTap: () => BlocProvider.of<ContentCubit>(context)
+                              .deleteContent(item.id),
+                          child: BuildTimerContainer(
+                            subject: item.subject,
+                            time: "${item.time}dk",
+                            iconPath: item.iconPath,
+                            containerBg: item.containerBgColor,
+                          ),
                         ),
                       );
                     },
@@ -64,6 +69,105 @@ class Home extends StatelessWidget {
             ),
           );
         },
+      ),
+      floatingActionButton: FloatingActionButton(
+          backgroundColor: AppColors.addButtonBgColor,
+          child: const Icon(
+            Icons.add,
+            color: AppColors.addButtonIconColor,
+          ),
+          onPressed: () {
+            showDialog(
+                context: context,
+                builder: (context) => _buildShowDialog(
+                    context, subjectController, timeController));
+          }),
+    );
+  }
+
+  Widget _buildShowDialog(
+      BuildContext context,
+      TextEditingController subjectController,
+      TextEditingController timeController) {
+    return Dialog(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+      backgroundColor: AppColors.homeBgColor,
+      child: SingleChildScrollView(
+        child: SizedBox(
+          width: 500,
+          height: 300,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "Etkinlik Ekle",
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500),
+              ),
+              const SizedBox(height: 20),
+              _buildTextField(subjectController, "subject"),
+              const SizedBox(height: 20),
+              _buildTextField(timeController, "time"),
+              const SizedBox(height: 20),
+              _buildAddButton(() {
+                final newContent = ContentModel(
+                    id: 4,
+                    subject: subjectController.text,
+                    time: int.parse(timeController.text),
+                    iconPath: "assets/icon/earth.png",
+                    containerBgColor: AppColors.timerContainerColor2);
+                BlocProvider.of<ContentCubit>(context).addContent(newContent);
+                Navigator.pop(context);
+              })
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTextField(TextEditingController controller, String text) {
+    return Container(
+      width: 200,
+      height: 50,
+      decoration: BoxDecoration(
+          border: Border.all(), borderRadius: BorderRadius.circular(20)),
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextField(
+                controller: controller,
+                decoration: InputDecoration(
+                    border: InputBorder.none,
+                    focusedBorder: InputBorder.none,
+                    enabledBorder: InputBorder.none,
+                    errorBorder: InputBorder.none,
+                    disabledBorder: InputBorder.none,
+                    hintText: text),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildAddButton(Function() onTap) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: 80,
+        height: 40,
+        decoration: BoxDecoration(
+            color: AppColors.addButtonBgColor,
+            borderRadius: BorderRadius.circular(10)),
+        child: const Center(
+            child: Text(
+          "Ekle",
+          style: TextStyle(color: AppColors.addButtonIconColor),
+        )),
       ),
     );
   }
